7c6cc561e797bb84e7f5b2ed258423a0814b4dcc
Anita <ahlistopad@gmail.com>
2022-05-23 22:01:31 +0200

    'initial commit'

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Handler from "./components/Handler";
+import LogInPage from "./components/LogInPage";
+function App() {
+  return (
+    <>
+      <LogInPage />
+    </>
+  );
+}
+
+export default App;
