55b8b7612b75de4f98139af004c6249312a09d3c
Anita <ahlistopad@gmail.com>
2022-05-25 20:58:49 +0200

    routes added

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
 import Error from "./pages/Error";
+import Home from "./pages/Home";
 function App() {
   return (
     <>
       <MyAppBar />
       <Router>
         <Routes>
-          <Route path="/" element={<LogInPage />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<LogInPage />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
           <Route path="/portfolio" element={<Portfolio />}></Route>
           <Route path="*" element={<Error />}></Route>
--- src/components/MyAppBar.tsx
+++ src/components/MyAppBar.tsx
@@ -7,13 +7,10 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 
-const pages = ['Home', 'About', 'Profile', 'Portfolio'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const pages = ['Home', 'About', 'Profile', 'Portfolio', 'Login'];
 
 const ResponsiveAppBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
