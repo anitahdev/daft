beec8225ba3943bf89b3140972965c7072e06a2e
Anita <ahlistopad@gmail.com>
2022-05-30 00:23:41 +0200

    landing page title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,20 @@ import React from "react";
 
 function Home() {
   return (
-    <div
-      style={{
-        backgroundImage: `url("https://images.unsplash.com/photo-1617791160505-6f00504e3519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1856&q=80")`,
-        width: 1265,
-        height: 650,
-      }}
-    >
-      Hello World
-    </div>
+    <>
+      <div style={{ marginLeft: 50, color: 'purple' }}>
+        <h1>welcome to my website</h1>
+      </div>
+      <div
+        style={{
+          fontSize: 46,
+          color: "white",
+          backgroundImage: `url("https://images.unsplash.com/photo-1617791160505-6f00504e3519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1856&q=80")`,
+          width: 1265,
+          height: 650,
+        }}
+      ></div>
+    </>
   );
 }
 
