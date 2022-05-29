a4a3df1799013f3e4f75b20ff44cde584b71b529
Anita <ahlistopad@gmail.com>
2022-05-29 23:54:29 +0200

    background-image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,15 @@
-import { width } from "@mui/system";
 import React from "react";
 
 function Home() {
   return (
     <div
-  
+      style={{
+        backgroundImage: `url("https://images.unsplash.com/photo-1617791160505-6f00504e3519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1856&q=80")`,
+        width: 1265,
+        height: 650,
+      }}
     >
-      welcome to my website
+      Hello World
     </div>
   );
 }
