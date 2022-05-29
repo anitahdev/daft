fe950d5af7bdefc5a327174325a4018fc743fd1f
Anita <ahlistopad@gmail.com>
2022-05-30 00:53:34 +0200

    about page done

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,30 +1,27 @@
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
+import Button from "@mui/material/Button";
 
 export default function About() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <div style={{ width: 500, height: 500, marginLeft: 80, marginTop: 200 }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nesciunt
+        molestias excepturi voluptatem quis deserunt obcaecati alias sit quo
+        deleniti, tempore cupiditate autem natus repellat! Autem modi natus quo
+        ipsum? Provident similique, laboriosam commodi, magnam sequi animi
+        voluptas quis rerum corporis, quae amet illum placeat tenetur magni
+        ipsam soluta incidunt.
+        <Button variant="text">Contact me</Button>
+      </div>
+      <div
+        style={{
+          width: 500,
+          height: 500,
+          marginLeft: 100,
+          marginTop: 50,
+          backgroundImage: `url("https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80")`,
+        }}
+      ></div>
+    </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 
+
 function Home() {
   return (
-    <div
-      style={{
-        backgroundImage: `url("https://images.unsplash.com/photo-1617791160505-6f00504e3519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1856&q=80")`,
-        width: 1265,
-        height: 650,
-      }}
-    >
-      Hello World
-    </div>
+    <>
+      <div style={{ marginLeft: 50, color: "purple" }}>
+        <h1>Welcome to my page</h1>
+      </div>
+      <div
+        style={{
+          backgroundImage: `url("https://images.unsplash.com/photo-1617791160505-6f00504e3519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1856&q=80")`,
+          width: 1265,
+          height: 650,
+        }}
+        
+      ></div>
+      
+    </>
   );
 }
 
