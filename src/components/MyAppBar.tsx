16a24e8310f96ec389e47d4071b216e911b665f4
Anita <ahlistopad@gmail.com>
2022-05-30 12:58:51 +0200

    added fixes

--- src/components/MyAppBar.tsx
+++ src/components/MyAppBar.tsx
@@ -17,18 +17,14 @@ const ResponsiveAppBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
     null
   );
-  
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
   };
-  
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
-
-
 
   return (
     <AppBar position="static">
